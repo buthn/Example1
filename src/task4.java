e9216833d2fbc3cf15ac6e52246efbf260fb0fb0
Buthaina <134599826+buthn@users.noreply.github.com>
2023-07-31 23:02:40 +0400

    Update task4.java

--- src/task4.java
+++ src/task4.java
@@ -65,4 +65,38 @@ public class task4 {
 
         }
 
+
+        public class SmallestMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // Place nums[i] at its correct position
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+        SmallestMissingPositive solution = new SmallestMissingPositive();
+
+        // Example usage:
+        int[] nums = {3, 4, -1, 1};
+        int result = solution.firstMissingPositive(nums);
+        System.out.println(result); // Output: 2
+    }
+}
+
+
     }}
